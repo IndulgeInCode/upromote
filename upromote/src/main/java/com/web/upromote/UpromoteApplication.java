51a2da6c0aa1ae378eee0c2f3e335ba986b9ed2d
fengxz <fengxz11@chinaunicom.cn>
2019-08-24 14:19:49 +0800

    init commit

---
+++ upromote/src/main/java/com/web/upromote/UpromoteApplication.java
@@ -0,0 +1,13 @@
+package com.web.upromote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpromoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpromoteApplication.class, args);
+    }
+
+}
